98ea2ac3f4f6bf90c58264f61261831969eca820
ChrGyoshev <103893083+ChrGyoshev@users.noreply.github.com>
2022-07-05 09:24:15 +0300

    Update 04.Extract Emails.py

--- 09.Regular Expressions - Exercise/04.Extract Emails.py
+++ 09.Regular Expressions - Exercise/04.Extract Emails.py
@@ -1,25 +1,5 @@
-command = input()
-receipt = 0
-final = 0
-while not command == "special" and not command == "regular":
-    price = float(command)
-    if price <0:
-        print("Invalid price!")
-        command = input()
-        continue
-    else:
-        receipt += price
-    command = input()
-
-if receipt <= 0:
-    print("Invalid order!")
-else:
-    if command == "special":
-        final = (receipt * 1.20) * 0.90
-    else:
-        final = receipt * 1.20
-    print(f"Congratulations you've just bought a new computer!\n"
-          f"Price without taxes: {receipt:.2f}$\n"
-          f"Taxes: {receipt * 0.20:.2f}$\n"
-          f"-----------\n"
-          f"Total price: {final:.2f}$")
+import re
+text = input()
+pattern = r"(^|(?<=\s))[a-zA-Z0-9]+[\.,_-]?[a-zA-Z0-9]+@[a-z]+-?[a-z]+(\.[a-z]+)+"
+validator = [object.group()for object in re.finditer(pattern, text)]
+print(*validator, sep="\n")
