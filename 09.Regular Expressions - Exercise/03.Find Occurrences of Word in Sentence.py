2dd1155f9a240b1cf095920922269bdf62fb2fc5
ChrGyoshev <103893083+ChrGyoshev@users.noreply.github.com>
2022-07-05 09:23:53 +0300

    Update 03.Find Occurrences of Word in Sentence.py

--- 09.Regular Expressions - Exercise/03.Find Occurrences of Word in Sentence.py
+++ 09.Regular Expressions - Exercise/03.Find Occurrences of Word in Sentence.py
@@ -1,5 +1,7 @@
 import re
 text = input()
-pattern = r"(^|(?<=\s))[a-zA-Z0-9]+[\.,_-]?[a-zA-Z0-9]+@[a-z]+-?[a-z]+(\.[a-z]+)+"
-validator = [object.group()for object in re.finditer(pattern, text)]
-print(*validator, sep="\n")
+word = input()
+
+pattern = rf"\b{word}\b"
+validator = re.findall(pattern, text, re.IGNORECASE)
+print(len(validator))
