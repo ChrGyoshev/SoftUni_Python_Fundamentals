2c8574aadfef4348a4afebc271cd6fd71be09e43
ChrGyoshev <103893083+ChrGyoshev@users.noreply.github.com>
2022-07-05 09:20:58 +0300

    Update 02.Match Phone Number.py

--- 09.Regular Expressions - .Lab/02.Match Phone Number.py
+++ 09.Regular Expressions - .Lab/02.Match Phone Number.py
@@ -1,7 +1,6 @@
 import re
+
 text = input()
-pattern = r"\b(?P<day>\d{2})(?P<sep>.|/|-)(?P<month>[A-Z][a-z]{2})(?P=sep)(?P<year>\d{4})\b"
-valid = [object.groupdict()for object in re.finditer(pattern, text)]
-for match in valid:
-    print(f'Day: {match["day"]}, Month: {match["month"]}, Year: {match["year"]}')
-a=123
+pattern = r"\+359(\s|-)2\1\d{3}\1\d{4}\b"
+valid = [obj.group() for obj in re.finditer(pattern,text)]
+print(*valid,sep = ", ")
