d0ffcb3a10a33dd207ccce9da9578d12400d9b5a
ChrGyoshev <103893083+ChrGyoshev@users.noreply.github.com>
2022-07-05 09:24:46 +0300

    Update 05.Furniture.py

--- 09.Regular Expressions - Exercise/05.Furniture.py
+++ 09.Regular Expressions - Exercise/05.Furniture.py
@@ -1,11 +1,16 @@
 import re
-text = input()
-pattern = r"w{3}\.[A-Za-z0-9-]+(\.[a-z]+)+"
-dict = []
-while text:
-    match = [object.group() for object in re.finditer(pattern,text)]
+pattern = r">>(?P<name>[A-Za-z]+)<<(?P<price>\d+\.?\d+?)!(?P<quantity>\d+)"
+command = input()
+names = []
+total_money = 0
+while not command =="Purchase":
+    match = re.match(pattern, command)
     if match:
-        dict.extend(match)
-    text = input()
-
-print(*dict, sep="\n")
+        data = match.groupdict()
+        names.append(data["name"])
+        total_money += float(data["price"]) * int(data["quantity"])
+    command = input()
+print("Bought furniture:")
+for el in names:
+    print(el)
+print(f'Total money spend: {total_money:.2f}')
