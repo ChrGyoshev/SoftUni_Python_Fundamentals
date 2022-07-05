9eb768f8e28a5145f23e5f89ada3792d16453f87
ChrGyoshev <103893083+ChrGyoshev@users.noreply.github.com>
2022-07-05 09:17:29 +0300

    Update 04. Students.py

--- 07.Dictionaries - .Lab/04. Students.py
+++ 07.Dictionaries - .Lab/04. Students.py
@@ -1,1 +1,28 @@
+word = input().split(":")
+dict = {}
+while not len(word) < 2:
+    name,id,course = word[0],word[1],word[2]
+    if course not in dict:
+        dict[course] = {"name": [name], "id":[id]}
+    else:
+        dict[course]["name"].append(name)
+        dict[course]["id"].append(id)
+
+    word = input().split(":")
+word= word[0].split("_")
+word = str(" ".join(word))
+names = []
+ids = []
+for key in dict:
+    if key == word:
+        names.append(dict[key]["name"])
+        ids.append(dict[key]["id"])
+
+for i in names:
+    for j in ids:
+        for q in range(len(i)):
+            print(f"{i[q]} - {j[q]}")
+
+
+
 
